d3207bb9412bf8bc02c714436e46b9b85df6ed1a
Matthew Lewis <matt@mplewis.com>
2014-07-28 00:32:42 -0700

    Add readonly DB for SQLite; provide a readonly POST interface for DB queries

--- sample_config.py
+++ sample_config.py
@@ -1,1 +1,2 @@
 DB_URI = 'sqlite:///db/data.sqlite'
+DB_URI_READ_ONLY = 'file:db/data.sqlite?mode=ro'
--- server/server.py
+++ server/server.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+import sys
+import os
+parent_dir = os.path.dirname(os.getcwd())
+sys.path.insert(0, parent_dir)
+
+
+import config
+
+import sqlite3
+from flask import Flask, request, jsonify
+
+
 app = Flask(__name__)
 
+os.chdir(parent_dir)
+print(os.getcwd())
+conn = sqlite3.connect(config.DB_URI_READ_ONLY, uri=True)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/', methods=['POST'])
+def sql_query():
+    query = request.form['query']
+    results = conn.execute(query)
+    column_names = [n[0] for n in results.description]
+    named_results = []
+    for row in results:
+        named_row = {}
+        for num, cell in enumerate(row):
+            named_row[column_names[num]] = cell
+        named_results.append(named_row)
+    output = {'rows': named_results}
+    return jsonify(output)
 
 
 if __name__ == '__main__':
