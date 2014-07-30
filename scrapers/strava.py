15c771701d53395987e6f178db321a711a0ed3bf
Matthew Lewis <matt@mplewis.com>
2014-07-29 19:06:36 -0700

    Strava scraper: open web browser; clean up strings

--- scrapers/strava.py
+++ scrapers/strava.py
@@ -4,6 +4,7 @@ scheduler = globals()['scheduler']
 def scrape_strava():
     import config
 
+    import webbrowser
     from cgi import parse_qs
     from wsgiref.simple_server import make_server
 
@@ -21,8 +22,9 @@ def scrape_strava():
     ACTIVITIES_TABLE = 'strava_activities'
     TOKEN_TABLE = 'strava_token'
 
-    # Strava auth callbacks will hit this port. Change it if you need to avoid
-    # port conflicts with other servers.
+    # Strava auth callbacks will hit this host and port. Change the port if you
+    # need to avoid port conflicts with other servers.
+    CALLBACK_HOST = 'http://localhost'
     CALLBACK_PORT = 36724
 
     # And here's the rest of the scraper.
@@ -30,8 +32,9 @@ def scrape_strava():
     def get_new_token():
         authorize_url = client.authorization_url(
             client_id=CLIENT_ID,
-            redirect_uri='http://localhost:%s' % CALLBACK_PORT
+            redirect_uri=('%s:%s' % (CALLBACK_HOST, CALLBACK_PORT))
         )
+        webbrowser.open(authorize_url)
         print('Click here to authorize me to access your Strava activities: %s'
               % authorize_url)
         make_server('localhost', CALLBACK_PORT, auth_listener).handle_request()
@@ -40,7 +43,8 @@ def scrape_strava():
         start_response("200 OK", [('Content-Type', 'text/plain')])
         parameters = parse_qs(environ.get('QUERY_STRING', ''))
         code_received(parameters['code'][0])
-        return ['Got your Strava authorization! You can close this window now.'
+        return ['Got your authorization from Strava! '
+                'You can close this window now.'
                 .encode('utf-8')]
 
     def code_received(code):
@@ -58,9 +62,12 @@ def scrape_strava():
         return pace.get_num()
 
     def query_strava():
+        print('Querying Strava...')
+
         table = db[ACTIVITIES_TABLE]
         activity_summaries = [a for a in client.get_activities()]
         num_added = 0
+
         for a_summary in activity_summaries:
             if table.find_one(strava_id=a_summary.id):
                 break
@@ -78,6 +85,7 @@ def scrape_strava():
             print('Added %s: %s' % (a.type, a.name))
             num_added += 1
             table.insert(activity_data)
+
         print('Done! %s activities added.' % num_added)
 
     client = Client()
