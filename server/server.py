4f57bed48756a24cb219794a0599a059aea9fcb1
Matthew Lewis <matt@mplewis.com>
2014-08-02 15:49:29 -0700

    Add docstrings to server.py

--- server/server.py
+++ server/server.py
@@ -27,12 +27,14 @@ os.chdir(parent_dir)
 
 
 def md5(input):
+    """Return an MD5 hash of the input data."""
     h = hashlib.md5()
     h.update(input.encode('utf-8'))
     return h.digest()
 
 
 class QueryResult:
+    """Implements the fields necessary for query caching."""
     def __init__(self, query_hash, result):
         self.query_hash = query_hash
         self.result = copy(result)
@@ -42,6 +44,11 @@ class QueryResult:
 
 
 class QueryResultsCache:
+    """
+    Cache SQL query results. If a query is made two or more times within
+    `expiry` seconds, the first result will be cached and returned for
+    subsequent results.
+    """
     def __init__(self, expiry=60):
         self.expiry = expiry
         self._queries = {}
@@ -71,6 +78,7 @@ cache = QueryResultsCache(expiry=config.QUERY_CACHE_EXPIRY_SECS)
 
 
 def query_processor(conn, query, result_queue):
+    """Return results for an SQL query with the time the queries took."""
     try:
         start_time = time()
         results = conn.execute(query)
@@ -92,6 +100,13 @@ def query_processor(conn, query, result_queue):
 
 
 def query_results(query):
+    """
+    Synchronously return results for an SQL query and return them as a dict to
+    be sent back to clients.
+
+    If the query takes too long, kill the query process and return an error
+    instead.
+    """
     conn = sqlite3.connect(config.DB_URI_READ_ONLY, uri=True)
     result_queue = Queue()
 
@@ -117,6 +132,7 @@ def query_results(query):
 
 @app.after_request
 def allow_cors(response):
+    """Allow CORS on every response this server sends."""
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
     return response
@@ -124,12 +140,17 @@ def allow_cors(response):
 
 @app.route('/', methods=['GET'])
 def hello():
+    """Let curious users know what Narcissa is."""
     return ('Hello. I\'m a Narcissa server. <a href="https://github.com/'
             'mplewis/narcissa">Learn more</a>.')
 
 
 @app.route('/', methods=['POST'])
 def sql_queries():
+    """
+    Answer a bundle of SQL queries POSTED to the server with a bundle of query
+    results.
+    """
     try:
         results = {}
         for name, query in request.form.items():
